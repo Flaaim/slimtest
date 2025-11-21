4e24f20020cd9a0503493591ae8ac27a7be37972
Flaaim <flaaim@list.ru>
2025-11-21 14:06:38 +1000

    split commands

--- config/dev/console.php
+++ config/dev/console.php
@@ -3,12 +3,14 @@
 declare(strict_types=1);
 
 use Doctrine\ORM\Tools\Console\Command\ValidateSchemaCommand;
+use Doctrine\ORM\Tools\Console\Command\SchemaTool;
 
 return [
     'config' => [
         'console' => [
             'commands' => [
                 ValidateSchemaCommand::class,
+                SchemaTool\DropCommand::class,
             ]
         ]
     ]

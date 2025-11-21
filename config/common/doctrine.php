32051978f9894625ecdcf196dd508588647277d6
Flaaim <flaaim@list.ru>
2025-11-21 13:45:35 +1000

    Added manager configuration

---
+++ config/common/doctrine.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Types\Type;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
+use Doctrine\ORM\ORMSetup;
+use Psr\Container\ContainerInterface;
+
+
+return [
+    EntityManagerInterface::class => function (ContainerInterface $container) {
+        $settings = $container->get('config')['doctrine'];
+
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            $settings['metadata_dirs'],
+            $settings['dev_mode'],
+            $settings['proxy_dir'],
+        );
+        $config->setNamingStrategy(new UnderscoreNamingStrategy());
+
+        foreach ($settings['types'] as $name => $class) {
+            if (!Type::hasType($name)) {
+                Type::addType($name, $class);
+            }
+        }
+
+        $connection = DriverManager::getConnection($settings['connection'], $config);
+
+        return new EntityManager($connection, $config);
+    },
+    'config' => [
+        'doctrine' => [
+            'dev_mode' => false,
+            'cache_dir' => __DIR__ . '/../../var/cache/doctrine/cache',
+            'proxy_dir' => __DIR__ . '/../../var/cache/doctrine/proxy',
+            'connection' => [
+                'driver' => 'pdo_mysql',
+                'host' => getenv('DB_HOST'),
+                'user' => getenv('DB_USER'),
+                'password' => getenv('DB_PASSWORD'),
+                'dbname' => getenv('DB_NAME'),
+                'charset' => 'utf8mb4',
+                'driverOptions' => [
+                    1002 => "SET NAMES 'utf8mb4'",
+                ]
+            ],
+            'metadata_dirs' => [],
+            'types' => []
+        ]
+    ]
+];
